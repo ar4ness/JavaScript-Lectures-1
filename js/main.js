41a39f36401002fc575af0a4c1ee627643e99ad3
nisox <nisoxx@gmail.com>
2016-12-13 20:24:45 +0200

    Commit1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//alert("Hello World!");
+//console.log("Hello World!"); to console
+//it's a method for breaking a code
+//debugger;
+//var x = "My x";
+var x = "My x";
+x = "Another x";
+console.log(x);
