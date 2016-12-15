c1fee40abce5fdfe899e60c3b707bbdb71f3fde2
nisox <nisoxx@gmail.com>
2016-12-15 19:50:19 +0200

    First commit before little breakout

---
+++ js/main.js
@@ -0,0 +1,83 @@
+// console.log("2 + 2 * 2 = ", 2 + 2 * 2); // 6
+// console.log("2 + 2 = ", 2 + 2); // 4
+// console.log("5 - 3 = ", 5 - 3); //2
+// console.log("5 * 3 = ", 5 * 3); //15
+// console.log("5 % 3 = ", 5 % 3); //2
+// var x = 25;
+// console.log("- x = ", - x); //-25
+// var y = '25';
+// console.log("y = ", y); // string "25"
+// console.log("typeof y = ", typeof y); // string
+// console.log("+ y = ", + y); //  number 25
+// console.log("typeof y=", typeof +y); // number
+
+// var z = "10";
+// z = +z;
+// console.log("typeof z=", typeof z); // number
+
+// console.log("5 > 3 =", 5 > 3); // true
+// console.log("5 < 3 =", 5 < 3); // false
+// console.log("3 >= 3 =", 3 >= 3); // true
+// console.log("3 <= 5 =", 3 <= 5); // true
+// console.log("5 != "5" =", 5!= "5"); // false
+// console.log("5 !== "5" =", 5!== "5"); // true
+// console.log("null == undefined =", null == undefined); // true
+// console.log("null === undefined =", null === undefined); // false
+// console.log(0 && 6); // false
+
+// function test (){
+	// return("test success");
+// }
+// console.log(true && test() && "test" && "end of ands list") // "end of ands list"
+// console.log(true && test() && 0 && "end of ands list") // 0
+// console.log(false && 6); // false
+
+// console.log(false || 6); // 6
+// console.log(true || 6); // true
+// console.log(false || "end" || 0); // "end"
+// console.log("start" || true || 0); // "start"
+
+// console.log(!0) // true;
+// console.log(!"") // true;
+// console.log('!"true because contains text"', !"true because contains text") // false
+// console.log("!\"true because \\ contains text\"", !"true because contains text") // false
+// console.log("!true =", !true) // false
+// console.log('!!"I am string and I want ot become bool" = ', !!"I am string and I want ot become bool") //true
+
+// var x = (3, 4, 5);
+// console.log(x); // 5
+
+// var x = 10, y = 5, z = x + y;
+// console.log(z); // 15
+// console.log("undefined + 25 =", undefined + 25); //NaN
+// console.log("NaN == NaN = ", NaN == NaN); // false
+// console.log("NaN === NaN = ", NaN === NaN); // false
+// console.log("typeof NaN = ", typeof NaN); // number
+// console.log("2 * (2 + 3) = ", 2 * (2 + 3)); // 10
+
+// Increment
+// var x = 10; 
+// x++; // 10
+// console.log(x); // 11
+// var y = 10; 
+// ++y; //11
+// console.log(y); // 11
+// console.log(x++); // 11
+// console.log(++y); // 12
+
+// Decrement
+// var x = 10; 
+// x--; // 10
+// console.log(x); // 9
+// var y = 10; 
+// --y; //9
+// console.log(y); // 9
+// console.log(x--); // 9
+// console.log(--y); // 8
+
+
+
+
+
+
+
