ca9efb911f67702b4765bf5d6f124363c36bef69
Ar4ness <ar4ness@mail.ru>
2017-01-14 09:17:27 +0200

    soe my marks added

--- js/main.js
+++ js/main.js
@@ -96,16 +96,52 @@
 // console.log((0.1 * 10 + 0.2 * 10)/10); //0.3
 // console.log(+(0.1 + 0.2).toFixed(10));// 0.3
 // console.log(Math.round((0.1 + 0.2)*10) /10);//0.3
-
 // console.log(9999999999999999); // 100000000000000000
 
 
 // Strings
+
+/* Your code will include strings which are very long. There are two ways you can do this:
+
+1)You can use the + operator to append multiple strings together, like this:
+let longString = "This is a very long string which needs " +
+
+                        "to wrap across multiple lines because " +
+
+                        "otherwise my code is unreadable.";
+
+2) you can use the backslash character ("\") at the end of each line to indicate t
+
+let longString = "This is a very long string which needs \
+
+       to wrap across multiple lines because \
+
+                        otherwise my code is unreadable.“;
+
+  About Comparing strings: String Objects are Type Object, not String! String objects are rarely used, so the following results might be surprising.
+
+  When comparing a number and a string, the string is converted to a number value. JavaScript attempts to convert the string numeric literal to a Number type value. First, a mathematical value is derived from the string numeric literal. Next, this value is rounded to nearest Number type value.*/
+
+// creating a string
 // var string = "string";
 // var string = 'string';
 // var string = "It\'s test string";
 // console.log('first line \n next line');// first line and on the second line will be nexy line;
 // console.log('\\'); // \
+
+
+/*charAt - Returns a string containing the character at the specified index. If there is no character at the index, charAt returns an empty string. The first character is located at index 0
+
+charCodeAt - Returns the Unicode value of the character at the specified index. If there is no character at the index, charCodeAt returns NaN (Not a Number).
+
+concat(string) - Concatenates its argument to the end of the string that invokes the method. The string invoking this method is not modified; instead a new String is returned. This method is the same as adding two strings with the string concatenation operator + (e.g., s1.concat( s2 ) is the same as s1 + s2).
+
+fromCharCode - Converts a list of Unicode values into a string containing the corresponding characters.
+
+Indexof(substring, index) - Searches for the first occurrence of substring starting from position index in the string that invokes the method. The method returns the starting index of substring in the source string or –1 if substring is not found. If the index argument is not provided, the method begins searching from index 0 in the source string.
+
+LastIndexf(Indexof(substring, index) - Searches for the last occurrence of substring starting from position index and searching toward the beginning of the string that invokes the method. The method returns the starting index of substring in the source string or –1 if substring is not found. If the index argument is not provided, the method begins searching from the end of the source string. */
+
 
 // console.log('test'.length); // 4
 // console.log('test   '.length); // 7  - JS output all symbols including 
@@ -121,17 +157,32 @@
 // x = x.toLowerCase(); 
 // console.log(x);// a and b
 
+
+// For working with substrins in JS there are 3 methods : substring, substr, slice
+
+// substring return string from position start to end
 var string = "test string";
-// console.log(string.substring(1, 4)); // est;
+// console.log(string.substring(1, 5)); // est;
+
+
+// substr return string from position start to set length
 
 // console.log(string.slice(1, 5)); // est;
 // console.log(string.substr(1, 5)); // est s;
 
+
+// substring return string from position start to end
+
 // console.log(string.substring(5, 1)); // est;
+
+/*slice( start, end ) Returns a string containing the portion of the string from index start through index end. If the end index is not specified, the method returns a string from the start index to the end of the source string. A negative end index specifies an offset from the end of the string starting from a position one past the end of the last character (so –1 indicates the last character position in the string).*/
+
 // console.log(string.slice(5, 1)); //  returns nothing - empty string
 // console.log(string.substring(5, -1)); // test  - if substring sees minus number - it takes from 0 to bigger number
 // console.log(string.slice(5, -1)); // strin - he counts minus numbers from the end of string;
-// console.log(string.slice(-5, -1)); // trin -
+// console.log(string.slice(-5, -1)); // strin -
+
+
 
 // console.log(string.indexOf('t')); //0;
 // console.log(string.lastIndexOf('t')); //6;
@@ -164,4 +215,4 @@ if (string.indexOf(x) != -1) {
 
 
 
-
+//split( string ) Splits the source string into an array of strings (tokens) where its string argument specifies the delimiter (i.e., the characters that indicate the end of each token in the source string). 
