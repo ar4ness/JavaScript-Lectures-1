d1f137955cc3e6b5b88183fdb4d1aad31e438427
Ar4ness <ar4ness@mail.ru>
2017-02-07 17:12:35 +0200

    something done

--- js/main.js
+++ js/main.js
@@ -75,10 +75,22 @@
 //console.log(hasTextBySample(string, /some/i));//false
 	
 	function getMatches(string, regExp) {
-		return regExp.match(string);
+		var regExp = new RegExp(regExp,"g");
+		return string.match(regExp);
+		//return regExp.match(string);
 	};
 	
-	function getPartedPhone(string) {};
+	function getPartedPhone(string) {
+	  var str = string.match(/\+?(\d{1,2})[^]?(\d{3})[^]?(\d{3}[^]?\d{2}[^]?\d{2})/);
+  if(!str){ throw("Invalid phone number")}
+  
+  var obj = {};  
+  obj.countryCode = str[1]
+  obj.cityCode = str[2]
+  obj.phone = str[3]
+    
+  return obj;	
+	};
 	
 	
 	function checkEveryLineMatch(string, regExp) {};
@@ -150,6 +162,20 @@
   // }
  // };
  
+ // // function getPartedPhone(string){
+  // // var re=/[0-9]/;
+  // // var arr=getMatches(string,re);
+  
+  // // var obj={};
+  // // obj.countryCode=arr[0]+arr[1];
+  // // obj.cityCode=arr[2]+arr[3]+arr[4];
+  // // obj.phone=arr[5]+arr[6]+arr[7]+arr[8]+arr[9]+arr[10]+arr[11];
+  // // return obj;
+ // // }
+ 
+ 
+ 
+ 
 // // function hasTextBySample(string, regExp) {
         // // return string.match(regExp)!==null; 
     // // };
@@ -187,52 +213,52 @@
 
 
 
-(function () {
-	api.addModule("dateServices", {
-		getDayByDate: getDayByDate,
-		isYearLeap: isYearLeap,
-		getWeekendsCount: getWeekendsCount,
-		getPayDate: getPayDate
-	});
+// (function () {
+	// api.addModule("dateServices", {
+		// getDayByDate: getDayByDate,
+		// isYearLeap: isYearLeap,
+		// getWeekendsCount: getWeekendsCount,
+		// getPayDate: getPayDate
+	// });
 	
-	//functions
+	// //functions
 	
-	 function getDayByDate(year, month, day) {
-			var day = new Date(year, month, day).getDay();
-			switch (day) {
-				case 0: return "sunday";
-				case 1: return "monday";
-				case 2: return "tuesday";
-				case 3: return "wednesday";
-				case 4: return "thursday";
-				case 5: return "friday";
-				case 6: return "suturday";
-			}
-	 };
-	//console.log(getDayByDate(2017, 0, 2));
-	
-	function isYearLeap(year) {
-		return year%4 == 0;
-	};
-	//console.log(isYearLeap(2017));//false
-	
+	 // function getDayByDate(year, month, day) {
+			// var day = new Date(year, month, day).getDay();
+			// switch (day) {
+				// case 0: return "sunday";
+				// case 1: return "monday";
+				// case 2: return "tuesday";
+				// case 3: return "wednesday";
+				// case 4: return "thursday";
+				// case 5: return "friday";
+				// case 6: return "suturday";
+			// }
+	 // };
+	// //console.log(getDayByDate(2017, 0, 2));
 	
 	// function isYearLeap(year) {
-  // var year = new Date(year,1, 29);
-  // return year.getMonth() === 1;
- // };
+		// return year%4 == 0;
+	// };
+	// //console.log(isYearLeap(2017));//false
 	
-	function getWeekendsCount(year, month) {
-		var day = new Date(year, month).getMonth()
-		if (getDayByDate(year, month) == 0 || getDayByDate(year, month) == 1)
+	
+	// // function isYearLeap(year) {
+  // // var year = new Date(year,1, 29);
+  // // return year.getMonth() === 1;
+ // // };
+	
+	// // function getWeekendsCount(year, month) {
+		// // var day = new Date(year, month).getMonth()
+		// // if (getDayByDate(year, month) == 0 || getDayByDate(year, month) == 1)
 		
-		return day+=day;
-	};
-	console.log(getWeekendsCount(2017, 1));
+		// // return day+=day;
+	// // };
+	// // console.log(getWeekendsCount(2017, 1));
 	
-	function getPayDate(year, month, notification) {};
+	// // function getPayDate(year, month, notification) {};
 	
-})();
+// })();
 
 
 
@@ -270,9 +296,230 @@
         // }
 
 
+// // function getDayByDate(year, month, day) {
+  // // var weekDay = new Date(year, month, day);
+  // // switch(weekDay.getDay()){
+   // // case 0: 
+    // // return "su";
+    // // break;
+   // // case  1: 
+    // // return "mo";
+    // // break;
+   // // case 2: 
+    // // return "tu";
+    // // break;
+   // // case 3: 
+    // // return "we";
+    // // break;
+   // // case 4: 
+    // // return "th";
+    // // break;
+   // // case 5: 
+    // // return "fr";
+    // // break;
+   // // case 6: 
+    // // return "sa";
+    // // break;
+   // // default: 
+   // // throw("wrong date");
+  // // }
+ // // };
+ 
+ // // function isYearLeap(year) {
+  // // var year = new Date(year,1, 29);
+  // // return year.getMonth() === 1;
+ // // };
+
+
+// function getDayByDate(year,month,day){
+  // var date=new Date(year,month,day);
+  // var day=date.getDay();
+  // switch (day) {
+   // case 0:return "Sunday";
+      // case 1:return "Monday";
+   // case 2:return "Tuesday";
+   // case 3:return "Wednesday";
+   // case 4:return "Thursday";
+   // case 5:return "Friday";
+   // case 6:return "Saturday";
+   // default: return "Invalid date"
+  // }
+ // }
+
+
+ // function isYearLeap (year) {
+  // var date = new Date(year, 2, 0);
+  // console.log(date);
+  // return (date.getDate() === 29);  
+ // };
+ 
+ // function isYearLeap(year){
+  // var date=new Date(year,2,5);
+  // var newDate=date.setDate(0);
+  // if(newDate.getDate==28){
+   // return false;
+  // }
+  // else{return true;}
+ // }
+ 
+ 
+ // api.addMethod("dateServices","getPayDate",function(year,month,notification){
+            // var d= new Date(year,month);
+            // var today = new Date();
+            // var daysCount = daysInMonth(month,year);
+            // fridays = [];
+            // for (var i = 1; i <= daysCount; i++) {    //looping through days in month
+                // var newDate = new Date(d.getFullYear(), d.getMonth(), i)
+            
+                // if (newDate.getDay() == 5) {   //if Friday
+                    // fridays.push(i);
+                // }
+                // if (fridays.length==2)
+                // break;
+            // }
+            // if(today.getFullYear()==year&&today.getMonth()==month){
+                // if(today.getDate()>fridays[1])
+                    // return "Payment already has been";
+                // else
+                    // notification(fridays[1]);
+            // } 
+
+
+        // })
+		
+		
+	// //Дмитрий Тарасенко:
+	// function getWeekendsCount (year, month) {
+  // var date = new Date(year, month + 1, 0);
+  // var counterWeekEnds = 0;
+  // for (var i = 1; i <= date.getDate(); i++) {
+   // if(getDayByDate(year, month, i) == "Saturday" || getDayByDate(year, month, i) == "Sunday"){
+    // counterWeekEnds++;
+   // }
+  // }
+  // return counterWeekEnds;
+  
+ // };
+// //Igor V	
+	// function getWeekendsCount(year, month) {
+  // var weekendsCount = 0;
+  // var daysInMonthCount = new Date(year, month + 1, 0).getDate();
+  // for (var i=1; i <= daysInMonthCount; i++) {
+   // if (getDayByDate(year, month, i) === 0 || getDayByDate(year, month, i) === 6) {
+    // weekendsCount++;
+   // }
+  // }
+  // return weekendsCount;
+ // };
+// //исправленный вариант	
+		
+		
+		
+		
+(function () {
+	api.addModule("commonServices", {
+		getTypeOf: getTypeOf,
+		isYearLeap: isYearLeap,
+		getWeekendsCount: getWeekendsCount,
+		getPayDate: getPayDate
+	});		
+		
+	function getTypeOf(elem) {
+		return {}.toString.call(elem).slice(8, -1);
+	}	
+	console.log(getTypeOf('elem'));
+	
+	// function combine(elem1, elem2, stringSeparator) {
+		// if (getTypeOf(elem1) == getTypeOf(elem2) && getTypeOf(elem1) == "string" ||getTypeOf(elem1) == "array") {
+			// return elem1.concat(elem2);
+		// }
+		// else if (getTypeOf(elem1) == getTypeOf(elem2) && getTypeOf(elem1) == "number") {
+			// return elem1 + elem2;
+		// }
+		// else if (getTypeOf(elem1) == getTypeOf(elem2) && getTypeOf(elem1) == "object") {
+		
+			// return {
+				// elem1: elem2
+			// } 
+		// }
+		// else {return elem1, elem2;}
+	// }
+
+// console.log(combine(23, 2, ";") );
+
+	
+	});		
+	
+	function getTypeOf(elem) {
+		return {}.toString.call(elem).slice(8, -1);
+	}	
+	
+	
+	function combine(elem1, elem2, stringSeparator) {
+		function getTypeOf(elem1) {
+		return {}.toString.call(elem1).slice(8, -1);
+		}
+
+		switch (getTypeOf(elem1)) {
+		case "Array": return elem1.concat(elem2);
+		case "Number": return elem1+elem2;
+		case "String": return elem1 + stringSeparator + elem2;
+		case "Object": return copy(elem1, elem2);
+		default: return elem1, elem2;
+		}
+	}
+
+console.log(combine([1, 2,3], [5, 6]) );//[1, 2, 3, 5, 6]
+console.log(combine(23, 44) );//67
+console.log(combine("here", "is", "|") );// here|is
+//console.log(combine({a: 5}, {b: "10"}) );
+	
+	
+function limitTo(element, limit) {
+	function getTypeOf(element) {
+	return {}.toString.call(element).slice(8, -1);
+}
+var start = Math.min.appply(limit);
+var end = Math.max.apply(Math, limit);
+switch (getTypeOf(element)) {
+	case "Array": return element.slice(start, end);
+	case "Number": return ;
+	case "String": return element.slice(start, end);
+	
+	default: return element;
+	}
+}
+console.log(limitTo("fjdhgjdgfgdj", [1,3,5]))	;
+	
+	
+function filterByField(array, field) {
+	function compareField(a, b) {
+		return a.field - b.field;
+	}
+	var newArray = [];
+	newArray.sort(compareField);
+	for (var i = 0; i < newArray.length; i++) {
+		return (newArray[i].field);
+	}
+}
+
+
+function findByField(array, field, value) {
+	
+}
+
+
+
+
+function findByField(array, field, direction) {
+	
+}
 
 
 
 
 
 
+
+
+	
