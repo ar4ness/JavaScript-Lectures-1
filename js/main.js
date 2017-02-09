0892250e39c8165beb9e7be944289238db7c552d
Ar4ness <ar4ness@mail.ru>
2017-02-09 07:10:19 +0200

    внесла некоторые примечания для аттрибуты методы навигация

--- js/main.js
+++ js/main.js
@@ -32,44 +32,56 @@ console.log(spanText.prev('div'));//[prevObject: n.fn.init[1], context: document
 console.log(spanText.next());//[input, prevObject: n.fn.init[1], context: document]
 console.log(spanText.next("input")); //[input, prevObject: n.fn.init[1], context: document]
 
+// ------Manipulations with attributes------
 
 // mainTitle.addClass('test');
 // console.log(mainTitle.hasClass('test'));// true;
+
+// ------$([selector]).addClass([className]); -- add class to all selected elements;
+
 // mainTitle.removeClass('test');
 // console.log(mainTitle.hasClass('test'));//false
+
+// ------$([selector]).removeClass([className]); -- remove class from all selected elements;
+
 // mainTitle.toggleClass('test');
+//------$([selector]).toggleClass([className]); -- add class to all selected elements if there is no such class or delete class from all selected elements if there is such element;
+
+//------$([selector]).hasClass([className]); -- return true if there is  such class or false;
 
 // console.log($("img").attr("src"));//images/logo.png
 // $("img").attr('src', "image/ggdhfshgdf.gif");
 // $("img").removeAttr("src");
 
+// //------$([selector]).attr([attributeName], [attributeValue]); -- return attribute of first checked element or set the value of attribute to all choosen elements;
+// //------$([selector]).removeAttr([attributeName]); -- delete attribute for all choosen elements;
 
 // //method prop
 // console.log($("img").prop("src"));
 // $("img").prop('src', "imag");//GET file:///D:/JS1/JavaScript-Lectures-1/imag net::ERR_FILE_NOT_FOUND
 // $("img").removeProp("src");
-
 // console.log(mainTitle[0]);
 
 
 
 // //jquery always get back array
-// console.log(mainTitle.html());
-// console.log(mainTitle[0].innerHTML);
+// console.log(mainTitle.html());//return string just with html with all elements inside
+// console.log(mainTitle[0].innerHTML);// the same - [0] - чтоб обратиться к обычному тэгу минуя обертку jqueryж
 
-// mainTitle.html("<div>text</div>");
-// mainTitle[0].innerHTML = "<div>text 2</div>";
+// mainTitle.html("<div>text</div>");//заменил весь html на див с тестом text
+// mainTitle[0].innerHTML = "<div>text 2</div>";// делает то же самое
 
-
+//$([selector]).html([html]);//возвращает вложенный html первого элемента или заменяет html всей выборки на переданный;
+//$([selector]).val([value]);//возвращает значение аттрибута value первого элемента или заменяет value всей выборки на переданный;
 
 
 //jquery can create tegs
 
-
-var clickableButton = $("<div>clickable Button</div>").css("color", "red").on("click", function () {alert("asd")});
+console.log($("<div>text</div>"));// [div]; ---просто создаст тэг, а потом этот тэг можно вставить в html
+var clickableButton = $("<div>clickable Button</div>").css("color", "red").on("click", function () {alert("asd")});// заменили
 mainTitle.find("span.text").html(clickableButton);
 
-
+//value - для элементов формы
 //value - in input it is text
 
 console.log($("input").val());// - -output is nothing
