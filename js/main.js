2291865a5353d71b26439591479ff8b7b328fb86
Ar4ness <ar4ness@mail.ru>
2017-02-10 18:44:19 +0200

    second exercize functional done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//------ 1 exercize
+// ---- functional
 function Animal(name, type, weight, maxSpeed) {
 	var name = name;
 	var type = type;
@@ -24,62 +26,131 @@ function Animal(name, type, weight, maxSpeed) {
 		}
 		else {return speed + " m/s"};
 	},
-	
 	this.coverDistance = function coverDistance(distance) {
 		
 		return  distance / parseFloat(this.run()) + " sec";
 	};
-	
 }
 
-var firstDog = new Animal ("Jack", "dog", "10", "40" );
-console.log(firstDog.getName());//jack
-console.log(firstDog.getType());//dog
-console.log(firstDog.getWeight());//10kg
-console.log(firstDog.setWeight(12));//12kg
-console.log(firstDog.getWeight());//12kg
+// var firstDog = new Animal ("Jack", "dog", "10", "40" );
+// console.log(firstDog.getName());//jack
+// console.log(firstDog.getType());//dog
+// console.log(firstDog.getWeight());//10kg
+// console.log(firstDog.setWeight(12));//12kg
+// console.log(firstDog.getWeight());//12kg
+// console.log(firstDog.run());//40 m/s
+// console.log(firstDog.run(30));//undefined
+// console.log(firstDog.coverDistance(90));//3
 
-console.log(firstDog.run());//40 m/s
-console.log(firstDog.run(30));//undefined
+//------prototypes
+function AnimalProto(name, type, weight, maxSpeed) {
+	this._name = name;
+	this._type = type;
+	this._weight = weight;
+	this._maxSpeed = maxSpeed;
+	this._speed = maxSpeed;
+}
+AnimalProto.prototype = {
+	getName: function () {return this._name},
+	getType: function () {return this._type},
+	getWeight: function () {return this._weight + "kg"},
+	setWeight: function (newWeight) {return this._weight = newWeight},
+	
+	run: function (newSpeed) {
+		if(newSpeed!==undefined) {
+			if(newSpeed <= 0) {
+				throw ("It's too low");
+			}
+			else if(newSpeed > this._maxSpeed) {
+				throw ("Too hight!");
+			} 
+			else {
+				this._speed = newSpeed;
+			}
+		}
+		else {return this._speed + " m/s"};
+	},
+	
+	coverDistance: function (distance) {
+		return  distance / parseFloat(this.run()) + " sec";
+	}
+}	
 
-console.log(firstDog.coverDistance(90));//3
+var secDog = new AnimalProto ("Jess", "dog", "20", "50" );
+
+// console.log(secDog.getName());//jess
+// console.log(secDog.getType());//dog
+// console.log(secDog.getWeight());//20kg
+// console.log(secDog.setWeight(18));//18
+// console.log(secDog.getWeight());//18kg
+
+// console.log(secDog.run());//50 m/s
+// secDog.run(40);//
+//console.log(secDog.run());
+// console.log(secDog.coverDistance(100));//2.5 sec
 
 
 
 
+//-----2 exercize
+//-----function
+function Bird (name, type, weight, maxSpeed, canFly, maxFlySpeed) {
+	Animal.apply(this, arguments);
+	var name = name;
+	var type = type;
+	var weight = weight;
+	var maxSpeed = maxSpeed;
+	var canFly = canFly;
+	var maxFlySpeed = maxFlySpeed;
+	var speed = maxSpeed;
+	var flySpeed = canFly ? maxFlySpeed : 0;
+	this.fly = function(newSpeed) {
+		if (newSpeed !== undefined) {
+			if (!canFly) {
+				throw ("error");
+			} else if(newSpeed <= 0) {
+				return "The bird is not moving";
+			}
+			else if(newSpeed > maxSpeed) {
+				return "Birds can't fly like a rocket";
+			} 
+			else {
+				flySpeed = newSpeed;
+			}
+		}
+		else {return flySpeed + " m/s"};
+		}
+	this.run = function run(newSpeed) {
+		if(newSpeed) {
+			if(newSpeed <= 0) {
+				return "error";
+			}
+			else if(newSpeed > maxSpeed) {
+				return "error";
+			} 
+			else {
+				speed = newSpeed;
+			}
+		}
+		else {return speed + " m/s"};
+	},
+	
+	this.coverDistance = function coverDistance(distance) {
+		if (flySpeed < speed) {
+			return  distance / parseFloat(this.run()) + " sec";
+		} else if (flySpeed > speed) {
+			return  distance / parseFloat(this.fly()) + " sec";
+		}
+	};
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var Robbin = new Bird ("J", "bird", "0.2", 0, "true", 20);
+console.log(Robbin.getName());//j
+console.log(Robbin.getType());//bird
+console.log(Robbin.getWeight());//0.2 kg
+console.log(Robbin.run());//60
+console.log(Robbin.fly());//90
+console.log(Robbin.coverDistance(100));
 
 
 function Bird (name, type, weight, maxSpeed, canFly, maxFlySpeed) {
@@ -90,23 +161,8 @@ function Bird (name, type, weight, maxSpeed, canFly, maxFlySpeed) {
 	var maxSpeed = maxSpeed;
 	var canFly = canFly;
 	var maxFlySpeed = maxFlySpeed;
+	var speed = maxSpeed;
 	var flySpeed = canFly ? maxFlySpeed : 0;
-	this.fly = function() {}
-	
-	this.coverDistance = function coverDistance(distance) {
-		
-		return  distance / parseFloat(this.run()) + " sec";
-	};
-}
-
-var Robbin = new Bird ("J", "bird", "0.2", "true", 90);
-console.log(Robbin.getName());//j
-console.log(Robbin.getType());//bird
-console.log(Robbin.getWeight());//0.2 kg
-console.log(Robbin.run());
-
-
-
 
 
 
